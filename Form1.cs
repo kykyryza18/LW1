9d94b1426e2283b6c53f32cc5faaf2dcb45534ac
kykyryza18 <mdasha478@gmail.com>
2022-02-25 11:42:15 +0200

    First Commit

---
+++ Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label1.Hide();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            label1.Show();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Random random = new Random();
+            this.BackColor = Color.FromArgb(random.Next(255), random.Next(255), random.Next(255));
+            label1.Text = "Начало работы";
+        }
+    }
+}
